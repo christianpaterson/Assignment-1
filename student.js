e5a4cb5630c1e41317b2c49b2ae8a90927a0ec07
Chris <cpaterson152@gmail.com>
2023-06-07 11:39:51 -0700

    Add assignment 2

--- student.js
+++ student.js
@@ -1,3 +1,11 @@
+let aname = "Christian Paterson";
+let a_message = "cs190 JavaScript Lab 2";
+let code = 880;
+
+function displayinfo470() {
+  alert(`${aname} has completed ${a_message} and his code is ${code}.`);
+}
+
 function confirmlink872(linkname) {
   if (confirm("Are you sure you want to jump to " + linkname + "?")) {
     window.location = linkname;
